5499994f8b8cd26ee77dfd59bafd1551ecd4d5ec
Sariful Islam <sarifulislam.molla@gmail.com>
2020-10-10 03:05:15 +0530

    1st commit

---
+++ backup.py
@@ -0,0 +1,7 @@
+# import subprocess
+import os
+
+home_dir = os.system("mysqldump -u root -pZubizi03365656533 retech_main > retech.sql")
+
+
+# mysqldump = subprocess.call("mysqldump -u root -pZubizi03365656533 retech_main")
